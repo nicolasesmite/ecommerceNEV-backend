9e90e5114981700847e8b7981ef8ad47da322721
Nicolas Esmite <nicolasemanuelesmitevaldez@gmail.com>
2023-09-25 22:04:54 -0300

    bd finished

--- index.js
+++ index.js
@@ -3,3 +3,35 @@ const app = express();
 const cors = require("cors");
 const mercadopago = require("mercadopago");
 require("dotenv").config();
+
+mercadopago.configure({
+  access_token: process.env.ACCESS_TOKEN,
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Todo ok");
+});
+
+app.post("/create_preference", (req, res) => {
+  let preference = {
+    items: req.body.items,
+    back_urls: {
+      success: "",
+      failure: "",
+      pending: "",
+    },
+
+    auto_return: "approved",
+    shipments: {
+      cost: req.body.shipment_cost,
+      mode: "not_specified",
+    },
+  };
+});
+
+app.listen(8080, () => {
+  console.log("servidor corriendo");
+});
